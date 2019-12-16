2e6876549a714ab737808d13fb850ad030e94e3f
Larissa Pissurno <larissapissurno@gmail.com>
2019-12-16 01:19:50 -0400

    Tela inicial e navegação

---
+++ mobile/dancer-app/src/pages/Dancer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import ButtonBack from '../components/ButtonBack';
+
+export default function Dancer({ navigation }) {
+  return <ButtonBack navigation={navigation}></ButtonBack>
+}
